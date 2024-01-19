4350da5d0359046555f7e0313f865c97e72f76a7
luyaneluiz <luyane.luiz.ll@gmail.com>
2024-01-19 12:29:18 -0300

    feat: criando home e cards

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -12,6 +12,37 @@ export function Home() {
           Buscar
         </button>
       </section>
+
+      <h1 className="font-bold text-center mt-6 text-2xl mb-4">
+        Carros novos e usados em todo Brasil
+      </h1>
+
+      <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+        <section className="w-full bg-white rounded-lg">
+          <img
+            src=""
+            alt="Carro"
+            className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
+          />
+
+          <p className="font-bold mt-1 mb-2 px-2">BMW 320i</p>
+
+          <div className="flex flex-col px-2">
+            <span className="text-zinc-700 mb-6">
+              Ano 2016/2016 | 23.000 km
+            </span>
+            <strong className="font-medium text-black text-xl">
+              R$ 190.000,00
+            </strong>
+          </div>
+
+          <div className="w-ful h-px bg-slate-200 my-2"></div>
+
+          <div className="px-2 pb-2">
+            <span className="text-zinc-700">Campo Grande - MS</span>
+          </div>
+        </section>
+      </main>
     </Container>
   );
 }

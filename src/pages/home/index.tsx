f3f3469ea0196a46e3d56986429dc43ac25a84fb
luyaneluiz <luyane.luiz.ll@gmail.com>
2024-01-19 12:16:42 -0300

    feat: criando barra de busca

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,7 +3,15 @@ import { Container } from "../../components/container";
 export function Home() {
   return (
     <Container>
-      <h1>Home</h1>
+      <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
+        <input
+          placeholder="Digite o nome do carro..."
+          className="w-full border-2 rounded-lg h-9 px-3 outline-none"
+        />
+        <button className="bg-red-500 h-9 px-8 rounded-lg text-white font-medium text-lg">
+          Buscar
+        </button>
+      </section>
     </Container>
   );
 }

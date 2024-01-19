96bac7ccd1769f739f5ef29c71653ab4b3e2161f
luyaneluiz <luyane.luiz.ll@gmail.com>
2024-01-19 12:09:28 -0300

    feat: criando header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,32 @@
-import Logo from '../../assets/logo.svg'
+import Logo from "../../assets/logo.svg";
+import { Link } from "react-router-dom";
+import { FiUser, FiLogIn } from "react-icons/fi";
 
 export function Header() {
-    return (
-      <>
-       <img src={Logo} alt="" />
-      </>
-    )
-  }
-  
+  const signed = true;
+  const loadingAuth = false;
+
+  return (
+    <div className="w-full flex items-center justify-center h-16 bg-white drop-shadow-sm mb-4">
+      <header className="flex w-full items-center justify-between max-w-7xl px-4 mx-auto">
+        <Link to="/">
+          <img src={Logo} alt="Logo do site" />
+        </Link>
+
+        {!loadingAuth && signed && (
+          <Link to="/dashboard">
+            <div className="border-2 border-gray-900 rounded-full p-1">
+              <FiUser size={24} color="#000" />
+            </div>
+          </Link>
+        )}
+
+        {!loadingAuth && !signed && (
+          <Link to="/login">
+            <FiLogIn size={24} color="#000" />
+          </Link>
+        )}
+      </header>
+    </div>
+  );
+}

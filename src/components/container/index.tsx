4674735bffe1704fe55038915b6828f3f2de1eb3
luyaneluiz <luyane.luiz.ll@gmail.com>
2024-01-23 17:21:49 -0300

    style: estilizando container de login

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -1,5 +1,9 @@
 import { ReactNode } from "react";
 
 export function Container({ children }: { children: ReactNode }) {
-  return <div className="w-ful max-w-7xl mx-auto px-4">{children}</div>;
+  return (
+    <div className="w-ful h-screen max-w-7xl mx-auto px-4 flex justify-center items-center">
+      {children}
+    </div>
+  );
 }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -35,38 +35,42 @@ export function Login() {
 
   return (
     <Container>
-      <div className="w-full min-h-screen flex justify-center items-center flex-col gap-4">
-        <Link to="/" className="mb-6 max-w-sm w-full">
-          <img src={Logo} alt="Logo do site" className="w-full" />
-        </Link>
+      <div className="bg-white rounded-lg flex flex-col items-center w-3/4 p-8">
+        <div className="w-48 min-h-12 flex justify-center items-center flex-col gap-4">
+          <Link to="/" className="mb-6 max-w-sm w-full">
+            <img src={Logo} alt="Logo do site" className="w-full" />
+          </Link>
+        </div>
+
+        <form className="max-w-xl w-full p-4" onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-3">
+            <Input
+              type="email"
+              placeholder="Digite seu e-mail..."
+              name="email"
+              error={errors.email?.message}
+              register={register}
+            />
+          </div>
+
+          <div className="mb-3">
+            <Input
+              type="password"
+              placeholder="Digite sua senha..."
+              name="password"
+              error={errors.password?.message}
+              register={register}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="bg-zinc-900 w-full rounded-md text-white h-10 font-medium"
+          >
+            Acessar
+          </button>
+        </form>
       </div>
-
-      <form
-        className="bg-white max-w-xl w-full rounded-lg"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <div className="mb-3">
-          <Input
-            type="email"
-            placeholder="Digite seu e-mail..."
-            name="email"
-            error={errors.email?.message}
-            register={register}
-          />
-        </div>
-
-        <div className="mb-3">
-          <Input
-            type="password"
-            placeholder="Digite sua senha..."
-            name="password"
-            error={errors.password?.message}
-            register={register}
-          />
-        </div>
-
-        <button>Acessar</button>
-      </form>
     </Container>
   );
 }
